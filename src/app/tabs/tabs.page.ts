2e8541225aa38364abf886de4c2af3fac3200fe0
KarthickTechie <reachkarthickprogramer@gmail.com>
2024-11-08 22:50:38 +0530

    feat : Animated Tab button overlay

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, ContentChildren, EnvironmentInjector, inject, ViewChildren, AfterViewInit, ViewChild } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
@@ -8,12 +9,40 @@ import { triangle, ellipse, square } from 'ionicons/icons';
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel,NgClass],
 })
-export class TabsPage {
+export class TabsPage implements AfterViewInit {
   public environmentInjector = inject(EnvironmentInjector);
-
+  @ViewChildren(IonTabButton)tabButtons!:IonTabButton[];
+  selectedTab = ''
   constructor() {
     addIcons({ triangle, ellipse, square });
   }
+
+  ngAfterViewInit(){
+    console.log(this.tabButtons)
+    this.tabButtons.forEach((e)=>{
+      console.log(e)
+    })
+  }
+
+  tabChanged(){
+    
+    this.selectedTab =   this.tabButtons.find(e=>e.selected==true)?.tab as string
+  }
+  
+  setTransform(){
+    console.log('ngclass',this.selectedTab)
+    if(this.selectedTab === 'tab2'){
+      return 'selected-tab2'
+    }else if(this.selectedTab === 'tab3'){
+        return 'selected-tab3'
+    }else if(this.selectedTab === 'tab1'){
+      return 'selected-tab1'
+
+    }
+    
+    return ''
+  }
+  
 }

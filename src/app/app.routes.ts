ac989586e1a9e89c10f4dce900702d69728ecd3b
karthickTechie <reachkarthickprogramer@gmail.com>
2024-11-21 21:38:25 +0530

    feat : header and product card integration completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'header',
     loadComponent: () => import('./header/header.page').then( m => m.HeaderPage)
   },
   {
-    path: 'tabs',
+    path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
-
 ];
